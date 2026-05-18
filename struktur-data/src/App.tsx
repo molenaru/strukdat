7eb612a1041092743e1c2f5dc1aed5c943d4b157
molenaru <rajibjabar2311@gmail.com>
2026-05-18 14:49:30 +0700

    adding cdn

--- struktur-data/src/App.tsx
+++ struktur-data/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1>Kelompok 1</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
@@ -111,11 +111,16 @@ function App() {
             </li>
           </ul>
         </div>
+
+        
       </section>
 
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
+
+    
+
   )
 }
 
