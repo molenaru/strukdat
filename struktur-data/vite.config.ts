e9ae7c95ec85a5351a4ec29d3210c4d1e10f1df8
bagasyogta <bagasyogta223@gmail.com>
2026-06-06 18:31:31 +0700

    bug server 1

--- struktur-data/vite.config.ts
+++ struktur-data/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 4090,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4090,
+    allowedHosts: ['edustruct.saikeno.space'], // ✅ tambahkan host custom di sini
+  },
 })
